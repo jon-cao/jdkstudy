fd66f2a002dc5bf322e1ce7319a8b61f25b50aee
00055624 <caoxingming@laiyifen.com>
2020-08-26 13:05:17 +0800

    init project

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * TODO
+ *
+ * @author caoxingming
+ * @date 2020/8/26 11:42
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.println("----");
+    }
+}
